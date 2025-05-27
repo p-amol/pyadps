d226d8fafd04fd12c95e63eb2faa9d3c43509748
p-amol <prakashamol@gmail.com>
2025-05-27 19:11:18 +0530

    Removed restricted usage of mpl

--- src/pyadps/utils/plotgen.py
+++ src/pyadps/utils/plotgen.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Button, RadioButtons, Slider, TextBox
 from matplotlib.widgets import RectangleSelector
 
-mpl.use("TkAgg")
+# mpl.use("TkAgg")
 
 
 class CutBins:
